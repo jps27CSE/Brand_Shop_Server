c75d5b686a92fc2fb74b21da52c7142380d28a7d
jps27CSE <jackpritomsoren@hotmail.com>
2023-10-18 11:13:32 +0600

    added New Product adding functionality

--- index.js
+++ index.js
@@ -22,6 +22,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("insertDB");
+    const ProductCollection = database.collection("products");
+
+    app.post("/addProduct", async (req, res) => {
+      const product = req.body;
+      const result = await ProductCollection.insertOne(product);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -29,7 +38,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
